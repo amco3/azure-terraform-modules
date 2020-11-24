08212c5dc38091864a9d275d16dc47cee00b94e1
amco3 <71735897+amco3@users.noreply.github.com>
2020-11-24 10:08:35 -0500

    Update cluster.tf

--- aks_cluster/cluster.tf
+++ aks_cluster/cluster.tf
@@ -128,7 +128,7 @@ resource "azurerm_kubernetes_cluster" "cluster" {
     var.additional_tags,
     {
       cluster-name  = var.cluster_name
-      module-source = "github.com/FairwindsOps/azure-terraform-modules/aks_cluster"
+      module-source = "github.com/amco3/azure-terraform-modules/aks_cluster"
       created-by    = "Terraform"
   })
 }
